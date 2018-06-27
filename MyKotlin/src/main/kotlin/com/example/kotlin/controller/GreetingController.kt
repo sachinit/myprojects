6353bfa5a493c4abb90db7aaa5fa64a4ea4aeaa0
sachin <sachinit@gmail.com>
2018-06-27 18:29:30 -0500

    [sachin] New commmit
    Signed-off-by: sachin <sachinit@gmail.com>

--- MyKotlin/src/main/kotlin/com/example/kotlin/controller/GreetingController.kt
+++ MyKotlin/src/main/kotlin/com/example/kotlin/controller/GreetingController.kt
@@ -12,6 +12,16 @@ class GreetingController {
     val counter = AtomicLong()
 
     @GetMapping("/greeting")
-    fun greeting(@RequestParam(value= "name", defaultValue = "World!") name: String)
-            = GreetingData(counter.incrementAndGet(), "Hello, $name")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World!") name: String): GreetingData {
+        for (x in 1..10 step 2) {
+            print(x)
+        }
+        println()
+        for (x in 9 downTo 0 step 3) {
+            print(x)
+        }
+        return  GreetingData(counter.incrementAndGet(), "Hello, $name")
+    }
+
+
 }
