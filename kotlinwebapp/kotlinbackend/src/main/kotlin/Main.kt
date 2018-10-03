fcdd51143704c706ea37a4540998cd88c2d2a1c2
sachin <sachinit@gmail.com>
2018-10-03 05:47:51 -0500

    [sachin] Updates for kotlin backend

--- kotlinwebapp/kotlinbackend/src/main/kotlin/Main.kt
+++ kotlinwebapp/kotlinbackend/src/main/kotlin/Main.kt
@@ -2,33 +2,40 @@
 
 package com.personal.demo
 
-import com.google.gson.Gson
-import org.jetbrains.ktor.application.call
-import org.jetbrains.ktor.host.embeddedServer
-import org.jetbrains.ktor.http.ContentType
-import org.jetbrains.ktor.netty.Netty
-import org.jetbrains.ktor.response.header
-import org.jetbrains.ktor.response.respondText
-import org.jetbrains.ktor.routing.get
-import org.jetbrains.ktor.routing.routing
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.gson.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import java.text.*
+import java.time.*
 
-fun main(args : Array<String>) {
-    embeddedServer(Netty, 8080) {
-        routing {
-            get("/api/ping/{count?}") {
-                var count: Int = Integer.valueOf(call.parameters["count"]?: "1")
-                if (count < 1) {
-                    count = 1
-                }
-                var obj = Array<Entry>(count, {i -> Entry("$i: Hello, World!")})
-                val gson = Gson()
-                var str = gson.toJson(obj)
-                call.response.header("Access-Control-Allow-Origin", "*")
-                call.respondText(str, ContentType.Application.Json)
+data class Model(val name: String, val items: List<Item>, val date: LocalDate = LocalDate.of(2018, 4, 13))
+data class Item(val key: String, val value: String)
 
-            }
+val model = Model("root", listOf(Item("A", "Apache"), Item("B", "Bing")))
+
+fun Application.main() {
+    install(DefaultHeaders)
+    install(Compression)
+    install(CallLogging)
+    install(ContentNegotiation) {
+        gson {
+            setDateFormat(DateFormat.LONG)
+            setPrettyPrinting()
         }
-    }.start(wait = true)
+    }
+    routing {
+        get("/v1") {
+            call.respond(model)
+        }
+        get("/v1/item/{key}") {
+            val item = model.items.firstOrNull { it.key == call.parameters["key"] }
+            if (item == null)
+                call.respond(HttpStatusCode.NotFound)
+            else
+                call.respond(item)
+        }
+    }
 }
-
-data class Entry(val message: String)
