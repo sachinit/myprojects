ab64f0e4367188fb8ef1a92c35c0df7f8e8b0afb
sachin <sachinit@gmail.com>
2018-10-03 05:50:25 -0500

    [sachin] deleting old files

--- kotlinwebapp_1/frontend/src/org/jetbrains/demo/thinkter/ReactMarkdown.kt
+++
@@ -1,9 +0,0 @@
-package org.jetbrains.demo.thinkter
-
-import react.*
-
-private val ReactMarkdown: dynamic = runtime.wrappers.require("react-markdown")
-
-class ReactMarkdownProps(var source: String = "") : RProps()
-
-object ReactMarkdownComponent : ReactExternalComponentSpec<ReactMarkdownProps>(ReactMarkdown)
